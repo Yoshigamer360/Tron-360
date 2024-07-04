6725f3330059fe41a0db5bff428ad48789c299bc
Yoshigamer360 <133695386+Yoshigamer360@users.noreply.github.com>
2024-07-04 18:01:51 +0100

    Create main.py
    Imported libraries and started the colors

---
+++ main.py
@@ -0,0 +1,19 @@
+# A remake of the classic arcade game Tron
+# Date 04/07/24
+# Created by: Yoshi Gamer 360
+
+# Player 1 uses WASD to move
+# Player 2 uses arrow keys to move
+
+# Libraries
+import pygame, time
+
+# Start game engine
+pygame.init()
+
+# Define colour variables
+colourBackground = (1, 4, 59)
+colourPlayer1 = (0, 255, 17)
+colourPlayer2 = (255, 234, 0)
+colourWalls = (0, 1, 10)
+colourText = (221, 222, 240)
